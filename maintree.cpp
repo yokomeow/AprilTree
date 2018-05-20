f1a784057ec064a6205ab7889dbc30d85fe95cc3
vladahub <32332255+vladahub@users.noreply.github.com>
2018-05-20 19:34:13 +0300

    Update maintree.cpp

--- maintree.cpp
+++ maintree.cpp
@@ -3,24 +3,56 @@
 #include "class_Tree.hpp"
 #include "class_C.hpp"
 
+class DebugTree : public SimpleTree
+{
+    void print()
+    {
+        tree_element* cur_element = this->root;
+        //fixheight(cur_element);
+        printSubTree(cur_element);
+    }
+
+    void printSubTree(tree_element* cur_element)
+    {
+        cerr << "Element: " << cur_element->value << " ";
+        cerr << "Height: " << cur_element->height << " ";
+        if(cur_element->parent != nullptr)
+            cerr << "Parent: " << cur_element->parent->value << " ";
+        else
+            cerr << "Parent: -- ";
+
+        if(cur_element->left != nullptr)
+            cerr << "Left: " << cur_element->left->value << " ";
+        else
+            cerr << "Left: -- ";
+
+        if(cur_element->right != nullptr)
+            cerr << "Right: " << cur_element->right->value << " ";
+        else
+            cerr << "Right: -- ";
+
+        cerr << endl;
+
+        if (cur_element-> left != nullptr)
+            printSubTree(cur_element->left);
+
+        if (cur_element-> right != nullptr)
+            printSubTree(cur_element->right);
+    }
+};
+
 int main()
 {
-    Container* c = new SimpleTree();
+    Container* c = new DebugTree();
 
     for(int i = 1; i < 10; i++)
         c->insert(i*i);
 
-    printf("\nTree after creation:\n");
+    cerr << "Tree after creation:" << endl;
     c->print();
 
-    if(c->exists(25))
-        cout << "Search for value 25: found" << endl;
-
-    if(!c->exists(111))
-        cout << "Search for value 111: not found" << endl;
-
     c->remove(25);
-    cout << "\nTree after deletion of the element:" << endl;
+    cerr << "Tree after deletion of the element:" << endl;
     c->print();
 
     delete c;
